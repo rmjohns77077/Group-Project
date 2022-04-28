e2d071669a151c79b2a73906cd93cc5799276e2a
AdamFASHOO <AdamChapman1994@gmail.com>
2022-04-28 15:11:51 -0400

    further progress

---
+++ Midterm Group Project/Consumable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore
+{
+    public enum ProductType
+    {
+        Food,
+        Drink
+    }
+
+    public class Consumable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public ProductType Category { get; set; }
+
+        public Consumable(string Name, string Description, double Price, ProductType Category)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Price = Price;
+            this.Category = Category;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Description: " + Description);
+            Console.WriteLine("Price: " + Price + " dollars.");
+        }
+
+    }
+}
