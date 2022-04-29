159bf5002dc20b0db101fe21ebec91e6e42bec16
AdamFASHOO <AdamChapman1994@gmail.com>
2022-04-29 11:42:24 -0400

    pre lunch work

--- Midterm Group Project/Payment.cs
+++ Midterm Group Project/Payment.cs
@@ -14,12 +14,66 @@ namespace CoffeeStore
     }
     public class Payment
     {
-        //Properties?
-        //Constructor?
-        //Methods !
-        public Payment(PaymentType Pay)
+        public Payment()
         {
-            
+        }
+
+        public void PayUs(string input, double total)
+        {
+            bool paid = false;
+
+            if (input.ToLower().Trim().Contains("cash"))
+            {
+                do
+                {
+                    Console.WriteLine($"Your total is {total}");
+                    Console.WriteLine("Please enter your payment:");
+
+                    double tender = double.Parse(Console.ReadLine());
+
+                    //can validate for acceptable parse here 
+
+                    double due = (total) - tender;
+                    
+                    if (due == 0)
+                    {
+                        Console.WriteLine("Thanks, you're all set!");
+                        break;
+                    }
+                    else if (due < 0)
+                    {
+                        double change = tender - total;
+                        Console.WriteLine($"Here's {Math.Round(change, 2)} back...");
+                        Console.WriteLine("Thanks, you're all set!");
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("I didn't understand that, please try again.");
+                        continue;
+                        
+                    }
+
+                }
+                while (paid = false); 
+            }
+          
+            /*
+            else if (input.Contains("credit"))
+            {
+
+            }
+            else if (input.Contains("check"))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("That payment type was not accepted, please try again.");
+            }
+            */
+
         }
 
 
